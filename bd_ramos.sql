98243316773b58211d04b9af2d09053c41421e23
Rybensx <anderson_arf@hotmail.es>
2023-05-22 08:17:39 -0600

    modificacion del archivo bd_ramos

--- bd_ramos.sql
+++ bd_ramos.sql
@@ -1,9 +1,9 @@
 create table Curso (
     curso_id serial not null,
-    curso_nombre varchar(50) not null,
-    curso_periodos varchar(50) not null,
-    fase_modulo varchar(50),
-    Instructor_nombre varchar(50) not null,
-    creditos_curso varchar(50),
+    curso_nombre VARCHAR(100) not null,
+    curso_periodos INTEGER(2) not null,
+    fase_modulo INTEGER(2) not null,
+    Instructor_nombre VARCHAR(100) not null,
+    creditos_curso DECIMAL(3,2),
     primary key (curso_id)
 )
